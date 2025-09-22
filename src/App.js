c055cbc5cd026ac97636e21525f7fdd2dbdc4a22
SHRIKA8306 <shrikasenthilkumar8@gmail.com>
2025-09-22 10:15:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi,I am Shrika S</h1>
+      <h3>This is my first deployment React Application</h3>
+    </div>
+  );
+}
+
+export default App;

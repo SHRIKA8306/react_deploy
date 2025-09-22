4c2f859c0519c4501422097cc152cfe0ecd48256
SHRIKA8306 <shrikasenthilkumar8@gmail.com>
2025-09-22 11:50:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Application</h1>
       <h1>Hi,I am Shrika S</h1>
       <h3>This is my first deployment React Application</h3>
     </div>
